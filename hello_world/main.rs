358deaaa9938d13a2ebd21f4d5e365584cdead0e
Andy Gillette <andy.gillette@gmail.com>
2021-01-26 13:53:58 -0800

    hello world

---
+++ hello_world/main.rs
@@ -0,0 +1,6 @@
+// The main function is special: it is always the first
+// code that runs in every executable Rust program.
+fn main() {
+    // this ! indicates a macro vs function
+    println!("Hello, world!");
+}
