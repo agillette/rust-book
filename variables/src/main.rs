855df5306ff02e9437c77d00a2adb837c208adfa
Andy Gillette <andy.gillette@gmail.com>
2021-01-29 15:45:34 -0800

    variables 3.1

---
+++ variables/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+    const MAX_POINTS: u32 = 100_000;
+    println!("The max points is: {}", MAX_POINTS);
+    let y = 5;
+    let y = y + 1;
+    let y = y * 2;
+    println!("The value of y is: {}", y);
+}
