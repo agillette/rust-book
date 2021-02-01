b9bd93b86c2b04964866a67eb637badfa1164148
Andy Gillette <andy.gillette@gmail.com>
2021-02-01 08:53:50 -0800

    3.5 control flow

---
+++ loops/src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    // ---------------
+    // basic while loop
+    // let mut number = 3;
+
+    // while number != 0 {
+    //     println!("{}!", number);
+
+    //     number -= 1;
+    // }
+
+    // println!("LIFTOFF!!!");
+
+    // ----------------
+    // loop through array
+    // let a = [10, 20, 30, 40, 50];
+
+    // for element in a.iter() {
+    //     println!("the value is: {}", element);
+    // }
+
+    // ----------------
+    // using a range
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
+}
